7520b092309941ea34d54b4b45858d99c3356a50
Egy_ember <kamufiok06@gmail.com>
2025-02-14 23:45:04 +0100

    it can scan characteristics now

--- btconfig.go
+++ btconfig.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
 	"slices"
@@ -24,16 +25,26 @@ type btdevs struct {
 }
 type MsgNewDev struct{}
 
+type conncetion struct {
+	connected       bt.Device
+	services        []bt.DeviceService
+	characteristics []bt.DeviceCharacteristic
+}
 type model struct {
 	presses []string
 	scan    bool
 	term    struct {
 		x, y int
 	}
-	adapter  *bt.Adapter
-	btdevs   *btdevs
-	selected int
-	err      error
+	adapter    *bt.Adapter
+	btdevs     *btdevs
+	cursor     int
+	cursorMax  int
+	selected   int
+	conncetion *conncetion
+	textInPut  bool
+	text       *string
+	err        error
 }
 
 func initialModel() model {
@@ -43,16 +54,95 @@ func initialModel() model {
 	m.scan = false
 	m.adapter = bt.DefaultAdapter
 	m.adapter.Enable()
-	m.selected = 0
+	m.cursorMax = 0
+	m.cursor = 0
+	m.selected = -1
+	m.conncetion = nil
 	m.btdevs = new(btdevs)
 	m.btdevs.add = make(chan btdev, 10)
 	m.btdevs.scanResults = make([]btdev, 0)
 	go m.btdevs.addResult()
+	m.text = nil
+	m.textInPut = false
 	return m
 }
 
 func (s model) Init() tea.Cmd {
 	return tea.ClearScreen
+}
+
+func (s model) parseKey(msg tea.KeyMsg) (model, tea.Cmd) {
+	if s.adapter == nil {
+		return s, nil
+	}
+	switch msg.String() {
+	case "ctrl+c", "q":
+		if s.conncetion != nil {
+			s.conncetion.connected.Disconnect()
+		}
+		return s, tea.Quit
+	case "s":
+		if s.scan {
+			s.adapter.StopScan()
+			s.scan = false
+		} else {
+			go func() {
+				err := s.adapter.Scan(s.scanCallback)
+				if err != nil {
+					s.scan = false
+					s.err = err
+				}
+			}()
+			s.scan = true
+		}
+	case "up":
+		if s.cursor > 0 {
+			s.cursor--
+		}
+	case "down":
+		if s.cursor < s.cursorMax {
+			s.cursor++
+		}
+	case "enter":
+		if s.err != nil {
+			s.err = nil
+			break
+		}
+		if s.selected == -1 {
+			s.selected = s.cursor
+			s.conncetion = new(conncetion)
+			var err error
+			s.conncetion.connected, err = s.adapter.Connect(s.btdevs.scanResults[s.selected].addr, bt.ConnectionParams{})
+			if err != nil {
+				s.err = err
+				s.selected = -1
+				break
+			}
+
+			serviceUUID := bt.NewUUID([16]byte{246, 35, 207, 46, 213, 119, 141, 146, 175, 79, 198, 129, 199, 180, 108, 235})
+			s.conncetion.services, err = s.conncetion.connected.DiscoverServices([]bt.UUID{serviceUUID})
+			if len(s.conncetion.services) != 1 && err == nil {
+				err = errors.New("worng number of services returned")
+			}
+			if err != nil {
+				s.conncetion.connected.Disconnect()
+				s.err = err
+				s.selected = -1
+				break
+			}
+			s.conncetion.characteristics, err = s.conncetion.services[0].DiscoverCharacteristics(nil)
+			if err != nil {
+				s.conncetion.connected.Disconnect()
+				s.err = err
+				s.selected = -1
+				break
+			}
+
+		}
+	default:
+		s.presses = append(s.presses, msg.String())
+	}
+	return s, nil
 }
 
 func (s model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -61,35 +151,14 @@ func (s model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		s.term.x = msg.Width
 		s.term.y = msg.Height
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "q":
-			return s, tea.Quit
-		case "s":
-			if s.scan {
-				s.adapter.StopScan()
-				s.scan = false
-			} else {
-				go func() {
-					err := s.adapter.Scan(s.scanCallback)
-					if err != nil {
-						s.scan = false
-						s.err = err
-					}
-				}()
-				s.scan = true
-			}
-		case "up":
-			if s.selected > 0 {
-				s.selected--
-			}
-		case "down":
-			if s.selected < len(s.btdevs.scanResults)-1 {
-				s.selected++
-			}
-		default:
-			s.presses = append(s.presses, msg.String())
+		if s.textInPut {
+		} else {
+			return s.parseKey(msg)
 		}
 	case MsgNewDev:
+		if s.selected == -1 {
+			s.cursorMax = len(s.btdevs.scanResults) - 1
+		}
 		return s, nil
 	}
 	return s, nil
@@ -114,7 +183,7 @@ func (s model) View() string {
 	st := s.RenderStatusBar()
 	st += s.RenderMainContent()
 	st += fmt.Sprintln(s.term.x, "x", s.term.y)
-	st += fmt.Sprintln(s.selected)
+	st += fmt.Sprintln(s.cursor)
 	st += "Button presses:\n"
 	for _, v := range s.presses {
 		st += v
@@ -157,15 +226,23 @@ func (s model) RenderMainContent() (b string) {
 		if err != nil {
 			panic(err)
 		}
-		b += str
+		b += str + "\n"
+		b += "press enter to clear\n"
 		return
 	}
-	data := make([][]string, len(s.btdevs.scanResults))
-	for k, v := range s.btdevs.scanResults {
-		data[k] = []string{v.name, v.addr.String()}
+	if s.selected == -1 {
+		data := make([][]string, len(s.btdevs.scanResults))
+		for k, v := range s.btdevs.scanResults {
+			data[k] = []string{v.name, v.addr.String()}
+		}
+		tableWith := []int{int(math.Floor(float64(s.term.x) / 2.0)), int(math.Ceil(float64(s.term.x) / 2.0))}
+		b += ansi.Table([]string{"Name", "Addres"}, data, tableWith, s.cursor)
+		return
 	}
-	tableWith := []int{int(math.Floor(float64(s.term.x) / 2.0)), int(math.Ceil(float64(s.term.x) / 2.0))}
-	b += ansi.Table([]string{"Name", "Addres"}, data, tableWith, s.selected)
+	for _, v := range s.conncetion.characteristics {
+		b += fmt.Sprintln(v)
+	}
+
 	return
 }
 
